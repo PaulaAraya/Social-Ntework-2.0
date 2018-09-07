edc9607f25f978592ec541b96c5dc2bccc71ffa0
PaulaAraya <paula.arayawilliams@gmail.com>
2018-09-07 20:22:19 -0300

    Configuracion Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+// Animaciones
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+// Material
+import {MaterialModule} from './angularMaterialComponent';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
